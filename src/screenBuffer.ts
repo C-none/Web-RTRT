a609edbc9e10311ffada2151eead969f841bb709
neeewbieee <3043818081@qq.com>
2024-04-09 18:18:05 +0800

    fix

--- src/screenBuffer.ts
+++ src/screenBuffer.ts
@@ -7,30 +7,26 @@ class BufferPool {
     vBuffer: GPUTexture;
 
     constructor(device: webGPUDevice) {
+        let originWidth = Math.floor(device.canvas.width / device.upscaleRatio);
+        let originHeight = Math.floor(device.canvas.height / device.upscaleRatio);
         this.currentFrameBuffer = device.device.createTexture({
-            size: {
-                width: device.canvas.width / device.upscaleRatio,
-                height: device.canvas.height / device.upscaleRatio,
-            },
+            size: { width: originWidth, height: originHeight, },
             format: 'rgba16float', dimension: '2d',
             usage: GPUTextureUsage.STORAGE_BINDING | GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_SRC,
         });
         this.previousFrameBuffer = device.device.createTexture({
-            size: {
-                width: device.canvas.width / device.upscaleRatio,
-                height: device.canvas.height / device.upscaleRatio,
-            },
+            size: { width: originWidth, height: originHeight, },
             format: 'rgba16float', dimension: '2d',
             usage: GPUTextureUsage.TEXTURE_BINDING | GPUTextureUsage.COPY_DST,
         });
         this.depthTexture = device.device.createTexture({
-            size: { width: device.canvas.width / device.upscaleRatio, height: device.canvas.height / device.upscaleRatio },
+            size: { width: originWidth, height: originHeight },
             format: "depth32float",
             usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
         });
         this.vBuffer = device.device.createTexture({
-            size: { width: device.canvas.width / device.upscaleRatio, height: device.canvas.height / device.upscaleRatio },
-            format: "rgba32uint",// baryCoord.yz, motionVec, primitiveID, albedo.rgba
+            size: { width: originWidth, height: originHeight },
+            format: "rgba32uint",// baryCoord.yz, primitiveID, motionVec
             usage: GPUTextureUsage.RENDER_ATTACHMENT | GPUTextureUsage.TEXTURE_BINDING,
         });
     }
