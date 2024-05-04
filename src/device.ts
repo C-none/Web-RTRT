8fe6ecb90ee9fa92b4f93435b469a7ab34a89f59
neeewbieee <3043818081@qq.com>
2024-05-04 19:46:47 +0800

    .

--- src/device.ts
+++ src/device.ts
@@ -4,7 +4,7 @@ class webGPUDevice {
     device: GPUDevice;
     context: GPUCanvasContext;
     format: GPUTextureFormat;
-    upscaleRatio: number = 1;
+    upscaleRatio: number = 2;
 
     async init(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
